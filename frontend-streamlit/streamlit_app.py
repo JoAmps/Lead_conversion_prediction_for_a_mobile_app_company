f7b7218c0431b2b6934f41c2f1e76e5b5f614b98
Hyacinth Ampadu <ampaduh@gmail.com>
2022-05-15 21:56:19 +0000

    streamlit app

--- frontend-streamlit/streamlit_app.py
+++ frontend-streamlit/streamlit_app.py
@@ -30,7 +30,7 @@ def run():
             }
 
     if st.button("Predict"):
-            response = requests.post("http://0.0.0.0:80/predict", json=data)
+            response = requests.post("http://127.0.0.1:8000/predict", json=data)
             prediction =response.text
             st.success(f"The prediction from model: {prediction}")
     
