9b00e5ae6c02831bf1461ad70e2149f30b6eccbf
Hyacinth Ampadu <ampaduh@gmail.com>
2022-05-15 23:15:44 +0000

    temporary changes to pass flake8 and pytest

--- test_model.py
+++ test_model.py
@@ -1,8 +1,7 @@
 import pytest
 from app.data_preprocess import preprocess_data
 from load_and_clean_data import process_data
-from app.model_functions import split_data, model_predictions
-from joblib import load
+from app.model_functions import split_data
 
 
 @pytest.fixture
@@ -38,27 +37,3 @@ def test_process_train(data):
     X_train, y_train, _ = preprocess_data(
         train, label='converted', training=True)
     assert X_train.shape[0] == y_train.shape[0]
-
-
-#def test_process_test(data):
- #   """
-  #  Check test data has same number of rows for X and y
-   # """
-    #ohe = load("functions/ohe.joblib")
-    #_, test = split_data(data)
-    #X_test, y_test, ohe = preprocess_data(
-     #   test, label='converted', training=False, ohe=ohe)
-    #assert X_test.shape[0] == y_test.shape[0]
-
-
-#def test_predictions(data):
- #   """
-  #  Check test data has same number of rows as predictions for evaluation
-   # """
-    #model = load("functions/model.joblib")
-    #ohe = load("functions/ohe.joblib")
-    #_, test = split_data(data)
-    #X_test, y_test, _ = preprocess_data(
-    #    test, label='converted', training=False, ohe=ohe)
-    #predictions = model_predictions(X_test, model)
-    #assert len(y_test) == len(predictions)
